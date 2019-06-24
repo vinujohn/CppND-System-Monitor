4087023b607eefca9eafb5657590ae77b7096e9f
Vinu <vinu_john@hotmail.com>
2019-06-24 14:36:49 +0000

    more helper functions

--- util.h
+++ util.h
@@ -1,24 +1,27 @@
 #include <string>
 #include <fstream>
+#include <iterator>
 
 // Classic helper function
 class Util {
 
-public:
-
-static std::string convertToTime ( long int input_seconds );
-static std::string getProgressBar(std::string percent);
-static void getStream(std::string path, std::ifstream& stream);
+    public:
+        static std::string convertToTime ( long int input_seconds );
+        static std::string getProgressBar(std::string percent);
+        static void getStream(std::string path, std::ifstream& stream);
+        static std::vector<std::string> getStringElements(std::string input);
+        static std::vector<std::string> getFileLines(std::string path);
 };
 
 std::string Util::convertToTime (long int input_seconds){
-long minutes = input_seconds / 60;
-long hours = minutes / 60;
-long seconds = int(input_seconds%60);
-minutes = int(minutes%60);
-std::string result = std::to_string(hours) + ":" + std::to_string(minutes) + ":" + std::to_string(seconds);
-return result;
+    long minutes = input_seconds / 60;
+    long hours = minutes / 60;
+    long seconds = int(input_seconds%60);
+    minutes = int(minutes%60);
+    std::string result = std::to_string(hours) + ":" + std::to_string(minutes) + ":" + std::to_string(seconds);
+    return result;
 }
+
 // constructing string for given percentage
 // 50 bars is uniformly streched 0 - 100 %
 // meaning: every 2% is one bar(|)
@@ -51,7 +54,29 @@ void Util::getStream(std::string path, std::ifstream& stream){
     stream.open (path, std::ifstream::in);
     if (!stream && !stream.is_open()){
         stream.close();
-        throw std::runtime_error("Non - existing PID");
+        throw std::runtime_error("Could not find file!");
     }
-    //return stream;
+}
+
+std::vector<std::string> Util::getStringElements(std::string input) {
+    std::istringstream buf(input);
+    std::istream_iterator<std::string> beg(buf), end;
+    std::vector<std::string> values(beg, end);
+    return values;
+}
+
+std::vector<std::string> Util::getFileLines(std::string path){
+    std::ifstream stream;
+    getStream(path, stream);
+
+    std::vector<std::string> ret;
+
+    std::string line;   
+    while (getline(stream, line)){
+        ret.push_back(line);
+    }
+
+    stream.close();
+
+    return ret;
 }
